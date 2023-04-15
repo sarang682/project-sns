f3999e05d5dea5f58771afb35e1534bfb95e1855
sarang682 <jms20914@gmail.com>
2023-04-15 16:59:46 +0900

    feat: 댓글 기능 테스트 작성

---
+++ src/main/java/com/project/sns/controller/request/PostCommentRequest.java
@@ -0,0 +1,14 @@
+package com.project.sns.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostCommentRequest {
+
+    private String comment;
+
+}
